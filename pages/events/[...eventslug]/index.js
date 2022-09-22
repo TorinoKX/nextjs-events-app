208b16847422825cbaa221539be690e7cb6fa79e
Zak Cobham-Davis <zakcdavis@gmail.com>
2022-09-22 15:10:02 +1000

    Add logic to handle eventSlug and filter out invalid slug

--- pages/events/[...eventslug]/index.js
+++ pages/events/[...eventslug]/index.js
@@ -1,4 +1,43 @@
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../../dummy-data";
+
 function FilteredEventsPage() {
+  const router = useRouter();
+
+  const filterData = router.query.eventSlug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid Filter</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length == 0) {
+    return <p>No Events Found</p>
+  }
+
+  
+
   return (
     <div>
       <h1>Filtered Events Page</h1>
